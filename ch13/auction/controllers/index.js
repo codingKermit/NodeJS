bbe3ab636e476f038b1bb559537311b0ec19bdb9
codingKermit <woojw6012@gmail.com>
2025-05-17 21:52:41 +0900

    ch13 SSE, ws 사용

--- ch13/auction/controllers/index.js
+++ ch13/auction/controllers/index.js
@@ -1,5 +1,7 @@
-const { Op } = require('sequelize');
+const { Op, where } = require('sequelize');
 const { Good } = require('../models');
+const User = require('../models/user');
+const Auction = require('../models/auction');
 
 exports.renderMain = async (req, res, next) => {
   try {
@@ -43,3 +45,81 @@ exports.createGood = async (req, res, next) => {
     next(error);
   }
 };
+
+exports.renderAuction = async (req,res,next)=>{
+  try {
+    const [good,auction] = await Promise.all(
+      [
+        Good.findOne({
+          where:{id:req.params.id},
+          include:{
+            model:User,
+            as:'Owner'
+          }
+        }),
+        Auction.findOne({
+          where:{GoodId:req.params.id},
+          include:{
+            model:User
+          },
+          order:[['bid','ASC']]
+        })
+      ] 
+    ) 
+
+    res.render('auction',{
+      title:`${good.name} - NodeAuction`,
+      good,
+      auction
+    })
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+
+};
+
+exports.bid = async(req,res,next)=>{
+  try {
+    const {bid,msg} = req.body;
+    const good = await Good.findOne({
+      where:{id:req.params.id},
+      include:{model:Auction},
+      order:[[{model:Auction},'bid','DESC']]
+    });
+
+    if(!good){
+      return res.status(404).send('해당 상품은 존재하지 않습니다.');
+    };
+
+    if(good.price >= bid){
+      return res.status(403).send('시작 가격보다 높게 입찰해야 합니다.');
+    }
+
+    if(new Date(good.createdAt).valueOf()+(24*60*60*1000) < new Date()){
+      return res.status(403).send('경매가 이미 종료되었습니다.');
+    }
+
+    if(good.Auctions?.[0]?.bid >= bid){
+      return res.status(403).send('이전 입찰가보다 높아야 합니다.');
+    }
+
+    const result = await Auction.create({
+      bid,
+      msg,
+      UserId: req.user.id,
+      GoodId: req.params.id
+    });
+
+    req.app.get('io').to(req.params.id).emit('bid',{
+      bid:result.bid,
+      msg:result.msg,
+      nick:req.user.nick
+    });
+
+    res.send('ok');
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+}
